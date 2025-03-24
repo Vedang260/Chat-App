d0fc6185068e0752d199c03e50e4f7130cac2ce6
Vedang <vedang2607@gmail.com>
2025-03-24 17:08:59 +0530

    otp generation is done

--- backend/src/utils/generateOTP.util.ts
+++ backend/src/utils/generateOTP.util.ts
@@ -1,3 +1,6 @@
-export const generateOTP: string = {
-    return 
-}
+export const generateOTP = (): { otp: string; otpExpirationTime: Date } => {
+    const otp = Math.floor(100000 + Math.random() * 900000).toString(); // Generate a 6-digit OTP
+    const otpExpirationTime = new Date(Date.now() + 5 * 60 * 1000); // Set expiration time as Date object
+
+    return { otp, otpExpirationTime };
+};
