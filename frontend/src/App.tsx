5416fe57261b02102038e785fb29325ba4cf580e
Vedang <vedang.wappnet@gmail.com>
2025-04-17 15:29:41 +0530

    Sign up page is created

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
+import SignUp from './pages/SignUp'
 
 function App() {
   
@@ -8,6 +9,7 @@ function App() {
      <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/signup" element={<SignUp />} />
         </Routes>
       </Router>
     </>
